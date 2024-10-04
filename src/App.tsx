dbcb6017a759104609897db002beeace59d775f5
drashtik.inexture <drashtik.inexture@gmail.com>
2024-10-04 15:55:33 +0530

    added routing and page not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 // import './App.css'
 import Dashboard from '@/layouts/Dashboard'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import PageNotFound from './common/error/PageNotFound';
 
 function App() {
   return (
     <>
-      <Dashboard />
+      <Router>
+        <Routes>
+          <Route element={<Dashboard />}>
+            <Route path="/" element={<Dashboard />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ src/common/error/PageNotFound.tsx
@@ -0,0 +1,8 @@
+
+const PageNotFound = () => {
+  return (
+    <div className='flex justify-center items-center font-bold text-5xl align-middle text-center h-screen'>Page Not Found</div>
+  )
+}
+
+export default PageNotFound
